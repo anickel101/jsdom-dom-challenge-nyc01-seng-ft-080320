cb0b14a0a8b97c52a3e48cb3b4f67d978eb9f46f
anickel101 <anson.nickel@gmail.com>
2020-09-16 15:35:29 -0400

    completed functionality

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,9 @@
+let intervalID = "";
+
 document.addEventListener("DOMContentLoaded", (e) => {
     console.log("DOMloaded");
 
-    let intervalID = setInterval(incrementCounter, 1000);
+    intervalID = setInterval(incrementCounter, 1000);
 });
 
 const incrementCounter = () => {
@@ -9,7 +11,7 @@ const incrementCounter = () => {
     count.innerText = parseInt(count.innerText) + 1;
 };
 
-const plusButton = document.getElementById('plus')
+const plusButton = document.getElementById('plus');
 
 plusButton.addEventListener("click",(e) => {
     incrementCounter();
@@ -20,34 +22,77 @@ const decrementCounter = () => {
     count.innerText = parseInt(count.innerText) - 1;
 };
 
-const minusButton = document.getElementById('minus')
+const minusButton = document.getElementById('minus');
 
 minusButton.addEventListener("click",(e) => {
     decrementCounter();
-} );
+});
 
 const currentCount = () => {
    return document.querySelector('#counter').innerText;
 };
 
-const heartButton = document.getElementById('heart')
+const heartButton = document.getElementById('heart');
+
 heartButton.addEventListener("click", (e) => {
 
-let likeUl = document.querySelector(".likes");  
+    let likeUl = document.querySelector(".likes");  
 
-let existLi = likeUl.querySelector(`[data-number='${currentCount()}']`);
-if (existLi) {
-    let numLikes = existLi.dataset.likes;
-    numLikes = parseInt(numLikes) + 1;
-    existLi.dataset.likes = numLikes;
-    existLi.innerText = `${existLi.dataset.number} has ${existLi.dataset.likes} likes.`;
-} else {
-    let likeLi = document.createElement("li");     
-    likeLi.dataset.number = currentCount();
-    likeLi.dataset.likes = 0;
-    likeLi.innerText = `${likeLi.dataset.number} has ${likeLi.dataset.likes} likes.`;
-    likeUl.append(likeLi);
-}; 
+    let existLi = likeUl.querySelector(`[data-number='${currentCount()}']`);
+    if (existLi) {
+        let numLikes = existLi.dataset.likes;
+        numLikes = parseInt(numLikes) + 1;
+        existLi.dataset.likes = numLikes;
+        existLi.innerText = `${existLi.dataset.number} has ${existLi.dataset.likes} likes.`;
+    } else {
+        let likeLi = document.createElement("li");     
+        likeLi.dataset.number = currentCount();
+        likeLi.dataset.likes = 0;
+        likeLi.innerText = `${likeLi.dataset.number} has ${likeLi.dataset.likes} likes.`;
+        likeUl.append(likeLi);
+    }; 
+});
 
+const pauseButton = document.getElementById('pause');
+pauseButton.addEventListener("click", (e) => {
 
+    if (pauseButton.innerText === "pause") {
+        clearInterval(intervalID)
+        pauseButton.innerText = "resume"
+        disableButtons()
+        pauseButton.disabled = false;
+    } else {
+        intervalID = setInterval(incrementCounter, 1000)
+        pauseButton.innerText = "pause"
+        enableButtons()
+    };
+       
+
+});
+
+const disableButtons = () => {
+    const getButtons = document.querySelectorAll("button");
+    for (const button of getButtons) {
+        button.disabled = true;
+    }
+};
+
+const enableButtons = () => {
+    const getButtons = document.querySelectorAll("button");
+    for (const button of getButtons) {
+        button.disabled = false;
+    }
+};
+
+const commentForm = document.getElementById("comment-form");
+commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const comment = form["comment"].value;
+    
+    const commentDiv = document.querySelector("div.comments");
+    const newComment = document.createElement("p");
+    newComment.innerText = comment;
+    commentDiv.append(newComment);
 });
